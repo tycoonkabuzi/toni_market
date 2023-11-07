b17e71ed961897b03eaf8b0338b24862737ed205
tycoonkabuzi <103669803+tycoonkabuzi@users.noreply.github.com>
2023-11-07 20:22:13 +0100

    other commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,8 +4,26 @@ import "../style/Nav.css";
 import searchLogo from "../assets/search.png";
 import bagLogo from "../assets/bag.png";
 import { useEffect, useState } from "react";
+
 function Nav() {
   const [scroll, setScroll] = useState(false);
+  const [active, setActive] = useState({
+    home: true,
+    collection: false,
+    brand: false,
+    blog: false,
+  });
+  function handleActiveness(item) {
+    setActive((prevElement) => {
+      const updateState = { ...prevElement, [item]: !prevElement[item] };
+      for (const element in updateState) {
+        if (element !== item) {
+          updateState[element] = false;
+        }
+      }
+      return updateState;
+    });
+  }
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
   }, []);
@@ -20,10 +38,30 @@ function Nav() {
     <nav className={scroll ? "scrolled" : ""}>
       <img className="logo" alt="logo" src={scroll ? logoWhite : logo} />
       <ul className="nav__links">
-        <li className="active">Home</li>
-        <li>Collection</li>
-        <li>Brand</li>
-        <li>Blog</li>
+        <li
+          onClick={() => handleActiveness("home")}
+          className={active.home ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => handleActiveness("collection")}
+          className={active.collection ? "active" : ""}
+        >
+          Collection
+        </li>
+        <li
+          onClick={() => handleActiveness("brand")}
+          className={active.brand ? "active" : ""}
+        >
+          Brand
+        </li>
+        <li
+          onClick={() => handleActiveness("blog")}
+          className={active.blog ? "active" : ""}
+        >
+          Blog
+        </li>
       </ul>
       <ul className="icons">
         <li>
