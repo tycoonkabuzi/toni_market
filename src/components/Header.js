7dd00497a09917311335a65b755e6cfbc68cc4da
tycoonkabuzi <103669803+tycoonkabuzi@users.noreply.github.com>
2023-11-10 19:16:43 +0100

    Currousel starting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,48 @@
 import "../style/Header.css";
-import shoe1 from "../assets/shoe1.png";
+
+import Data from "../database/headerData";
+import { useState } from "react";
+function toGray(word) {
+  const grayPart = word.text.split(" ", 2).join(" ");
+  return grayPart;
+}
+function toBlack(word) {
+  const inBlackArray = word.text.split(" ");
+  const inBlack = inBlackArray.slice(2).join(" ");
+  return inBlack;
+}
+
 function Header() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  function handleCurrousel(number) {
+    setCurrentIndex((prev) => {
+      let current = prev + number;
+      return current;
+    });
+  }
   return (
     <div className="main">
       <div className="mainText">
         <h1>
-          <span className="grayPart">Choose your</span> trendy collection
+          <span className="grayPart">{toGray(Data[currentIndex])} </span>
+          {toBlack(Data[currentIndex])}
         </h1>
         <div className="containerDots">
-          <span className="blue"></span>
-          <span className="purpule"></span>
-          <span className="red"></span>
-          <span className="green"></span>
+          <span className="blue" onClick={() => handleCurrousel(0)}></span>
+          <span className="purpule" onClick={() => handleCurrousel(1)}></span>
+          <span className="red" onClick={() => handleCurrousel(2)}></span>
+          <span className="green" onClick={() => handleCurrousel(3)}></span>
         </div>
       </div>
       <div>
-        <div className="containterShoe">
-          <img alt="shoe main showing front page " src={shoe1} />
+        <div className={`containterShoe ${Data[currentIndex].color}`}>
+          <img
+            alt="shoe main showing front page "
+            src={Data[currentIndex].pic}
+          />
           <div className="price">
-            <h1>$200.00</h1>
-            <p>$359.99</p>
+            <h1>${Data[currentIndex].price}</h1>
+            <p>${Data[currentIndex].oldPrice}</p>
           </div>
         </div>
       </div>
---
+++ src/database/headerData.js
@@ -0,0 +1,38 @@
+import shoe1 from "../assets/shoe1.png";
+import shoe2 from "../assets/shoe2.png";
+import shoe3 from "../assets/shoe3.png";
+const Data = [
+  {
+    id: 1,
+    text: "Choose your trendy collection ",
+    pic: shoe1,
+    price: 260,
+    oldPrice: 350,
+    color: " ",
+  },
+  {
+    id: 2,
+    text: "This is the new collection brand ",
+    pic: shoe2,
+    price: 560,
+    oldPrice: 850,
+    color: "containerGreen",
+  },
+  {
+    id: 3,
+    text: "Keep me on track easely",
+    pic: shoe3,
+    price: 340,
+    oldPrice: 500,
+    color: "containerPurpole",
+  },
+  {
+    id: 4,
+    text: "Can you pick what is right  ",
+    pic: shoe1,
+    price: 159.99,
+    oldPrice: 353.33,
+    color: " ",
+  },
+];
+export default Data;
