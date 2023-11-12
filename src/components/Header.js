60376a9555620d55d62e1617fea6da97896d6513
tycoonkabuzi <103669803+tycoonkabuzi@users.noreply.github.com>
2023-11-12 17:08:01 +0100

    change to currousel where we are now able to track what color

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,40 @@ import "../style/Header.css";
 
 import Data from "../database/headerData";
 import { useState } from "react";
-function toGray(word) {
-  const grayPart = word.text.split(" ", 2).join(" ");
-  return grayPart;
-}
-function toBlack(word) {
-  const inBlackArray = word.text.split(" ");
-  const inBlack = inBlackArray.slice(2).join(" ");
-  return inBlack;
-}
 
 function Header() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  function handleCurrousel(number) {
+  const [active, setActive] = useState({
+    blue: false,
+    purpule: false,
+    green: false,
+    red: false,
+  });
+  function toGray(word) {
+    const grayPart = word.text.split(" ", 2).join(" ");
+    return grayPart;
+  }
+  function toBlack(word) {
+    const inBlackArray = word.text.split(" ");
+    const inBlack = inBlackArray.slice(2).join(" ");
+    return inBlack;
+  }
+  function handleCurrousel(number, color) {
+    setActive((prev) => {
+      const updateState = { ...prev, [color]: !prev[color] };
+      for (const element in updateState) {
+        if (element !== color) {
+          updateState[element] = false;
+        }
+      }
+      return updateState;
+    });
     setCurrentIndex((prev) => {
       let current = prev + number;
       return current;
     });
   }
+
   return (
     <div className="main">
       <div className="mainText">
@@ -27,11 +43,23 @@ function Header() {
           <span className="grayPart">{toGray(Data[currentIndex])} </span>
           {toBlack(Data[currentIndex])}
         </h1>
-        <div className="containerDots">
-          <span className="blue" onClick={() => handleCurrousel(0)}></span>
-          <span className="purpule" onClick={() => handleCurrousel(1)}></span>
-          <span className="red" onClick={() => handleCurrousel(2)}></span>
-          <span className="green" onClick={() => handleCurrousel(3)}></span>
+        <div className="containerDots ">
+          <span
+            className={`blue ${active.blue ? "active" : ""}`}
+            onClick={() => handleCurrousel(1, "blue")}
+          ></span>
+          <span
+            className={`purpule ${active.purpule ? "active" : ""}`}
+            onClick={() => handleCurrousel(2, "purpule")}
+          ></span>
+          <span
+            className={`red ${active.red ? "active" : ""}`}
+            onClick={() => handleCurrousel(3, "red")}
+          ></span>
+          <span
+            className={`green ${active.green ? "active" : ""}`}
+            onClick={() => handleCurrousel(4, "green")}
+          ></span>
         </div>
       </div>
       <div>
